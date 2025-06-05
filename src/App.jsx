bb1117b4e2069ff9d6f33aa2abf6dd1914797b86
msncoder <msncoder1999@gmail.com>
2025-06-06 02:58:45 +0500

    chat model work done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,41 @@ import Chatmessage from "./components/Chatmessage";
 function App() {
   const [chatHistory, setChatHistory] = useState([]);
   // console.log(chatHistory);
-  const generateBotResponse = (chatHistory) => {
-    console.log(chatHistory);
+
+  const generateBotResponse = async (history) => {
+    const updateHistory = (text) => {
+      setChatHistory((prev) => [
+        ...prev.filter((msg) => msg.text !== "Thinking..."),
+        { role: "model", text },
+      ]);
+    };
+
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+    };
+
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_API_URL,
+        requestOptions
+      );
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.error.message || "something went wrong");
+      console.log(data);
+
+      const text =
+        data?.candidates?.[0]?.content?.parts?.[0]?.text ||
+        "Sorry, no response.";
+
+      updateHistory(text.replace(/\*\*(.*?)\*\*/g, "$1").trim());
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
